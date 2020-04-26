860ddf034681850db4eb064b45c5a0c98b2a7781
Bushra-B <bushraxbil@gmail.com>
2020-04-26 13:01:54 +0300

    sol

---
+++ server.js
@@ -0,0 +1,83 @@
+'use strict';
+
+// Load Enviroument Variables
+require('dotenv').config();
+
+//Server Dependencies
+const express = require('express');
+const superagent = require('superagent');
+const methodOverride = require('method-override');
+const pg = require('pg');
+
+//Main variables
+const app = express();
+const PORT = process.env.PORT;
+const client = new pg.Client(process.env.DATABASE_URL);
+
+//Uses
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+
+//Handle any route
+app.get('/', (request, response) => {
+  response.status(200).send('Working');
+});
+
+//Route definitions
+app.get('/home', homePageHandler);
+app.get('/fav', favoriteHandler);
+app.get('/favPage', favPageHandler);
+
+
+
+
+
+//Route Handlers
+
+//HomePage
+function homePageHandler(request, response) {
+  let url = 'https://digimon-api.herokuapp.com/api/digimon';
+  superagent.get(url)
+    .then(digimonResults => {
+      let digimonData = digimonResults.body.map(obj => {
+        return new Digimon(obj);
+      });
+      response.render('./pages/index', {data: digimonData});
+    });
+}
+
+//C.F
+function Digimon(obj){
+  this.name = obj.name;
+  this.img = obj.img;
+  this.level = obj.level;
+}
+
+//Favorite Page
+function favoriteHandler(request, response) {
+  let {name, img, level} = request.query;
+  let SQL = 'INSERT INTO digimons (name, img, level) VALUES ($1,$2,$3);';
+  let safeValues = [name, img, level];
+  client.query(SQL, safeValues)
+    .then(results => {
+    });
+  response.redirect('/favPage');
+}
+
+function favPageHandler(request, response) {
+    let SQL ='SELECT * FROM digimons;';
+    client.query(SQL)
+    .then(results => {
+        response.render('./pages/favorite', {data:results.rows});
+
+    });
+}
+
+
+
+// Listen on port
+app.listen(PORT, () => {
+  console.log('Listening on port: ', PORT);
+});
